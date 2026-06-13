6114badc431526608e8fa0985a233b849a9c75bf
Andrea Savastano <andreasavastano4388@gmail.com>
2026-06-13 11:51:31 +0200

    merge

--- source/actors/user.py
+++ source/actors/user.py
@@ -1,6 +1,7 @@
-from crypto.asymmetric import generate_rsa_key_pair
+from crypto.asymmetric import generate_rsa_key_pair, rsa_encrypt, rsa_decrypt, sign
 from models.certificate import Certificate
 from actors.certificationauthority import CertificationAuthority
+import random
 
 class User:
     def __init__(self, name:str, matriculation_number:str):
@@ -67,6 +68,34 @@ class User:
 
 
     # ========================== Fase Trasmissione voto ==========================
+    def create_Cfinal(self) -> bytes:
+        """ C_final = Enc_{pk_A}(s || c || c' || Cert(U)) """
+
+        v:str = random.choice(["SI", "NO"]) # Voto dell'utente, scelto casualmente per la simulazione
+        print(f"[User] Utente \"{self.name}\" ha scelto il voto: {v}")
+
+        pk_S = self.serverCert.getPublicKey()
+        pk_A = self.authenticatorCert.getPublicKey()
+
+        # Cifratura interna -> c = Enc_{pk_S}(v)
+        c:bytes = rsa_encrypt(pk_S, v.encode())
+
+        # Firma del cifrato -> sigma = Enc_{sk_U}(c)
+        sigma:bytes = sign(self.sk, c)
+
+        # Seconda cifratura voto -> c' = Enc_{pk_U}(v)
+        c_prime:bytes = rsa_encrypt(self.pk, v.encode())
+
+        # Cifratura esterna -> C_final = Enc_{pk_A}(s || c || c' || Cert(U))
+        cert_u_bytes:bytes = self.cert.to_bytes()
+        data_concatenated:bytes = sigma + c + c_prime + cert_u_bytes
+        C_final:bytes = rsa_encrypt(pk_A, data_concatenated)
+
+        print(f"[User] Utente \"{self.name}\" usa la pk_A per cifrare i dati ottenendo C_final=Enc_pkA(s || c || c' || Cert(U))")
+
+        return C_final
+
+
 
 
 
